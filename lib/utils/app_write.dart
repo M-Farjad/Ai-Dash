4559ae973cbd1bf03ffe1a1f3e6a8213e41cb661
Muhammad Farjad Waseem <2021SE56@student.uet.edu.pk>
2024-01-06 00:12:42 +0500

    AppWriteInit

---
+++ lib/utils/app_write.dart
@@ -0,0 +1,12 @@
+import 'package:appwrite/appwrite.dart';
+
+class AppWrite {
+  // void init() {
+  //   Client client = Client();
+  //   client
+  //           .setEndpoint('https://localhost/v1') // Your Appwrite Endpoint
+  //           .setProject('5e8cf4f46b5e8') // Your project ID
+  //           .setSelfSigned() // Use only on dev mode with a self-signed SSL cert
+  //       ;
+  // }
+}
